bb5bd0835e8f6e912a6861bb9a13479ded89368a
Lisa <lisa.roxane@gmail.com>
2021-12-01 17:00:36 -0330

    Moved css & js files to proper folders

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+alert('Hello world');
+let favoriteFood = 'Chocolate';
+document.write(favoriteFood);
--- scripts.js
+++
@@ -1,3 +0,0 @@
-alert('Hello world');
-let favoriteFood = 'Chocolate';
-document.write(favoriteFood);
