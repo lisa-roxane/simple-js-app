5b94a9b82a2aa292325a9ab984a1aec9c67e103f
Lisa <lisa.roxane@gmail.com>
2021-12-09 10:13:45 -0330

    Update scripts.js
    Add for loop to display name and height of each pokemon in the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
+// Pokemon list
 let pokemonList = [
     {name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
     {name: 'Beedrill', height: 1, types: ['bug', 'poison']},
     {name: 'Charizard', height: 1.7, types: ['fire', 'flying']}
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + " (height:" + pokemonList[i].height + ") " );
+  }
