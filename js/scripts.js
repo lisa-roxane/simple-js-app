52145e63f5d1ba61c3f813e34e694c5c8bb24da6
Lisa <lisa.roxane@gmail.com>
2021-12-20 10:16:50 -0330

    Update scripts.js
    Change for loop to forEach() function
    Wrap pokemonList in IIFE to convert array from global state
    Create pokemonRepository variable to hold what IIFE will return
    getAll and add functions assigned as keys to what the IIFE will return

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,22 @@
-// Pokemon list
+/* Create IIFE - wrap previous global variables (pokemonList) to make them local variables, 
+protecting them from changes */
+let pokemonRepository = (function () {
+// Pokemon list - an array of Pokémon objects used throughout the web app
 let pokemonList = [
-    {name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
-    {name: 'Beedrill', height: 1, types: ['bug', 'poison']},
-    {name: 'Charizard', height: 1.7, types: ['fire', 'flying']}
+    {
+        name: 'Bulbasaur', 
+        height: 7, 
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Beedrill', 
+        height: 1, 
+        types: ['bug', 'poison']
+    },
+    {
+        name: 'Charizard', 
+        height: 1.7, 
+        types: ['fire', 'flying']}
 ];
 
 // This for loop is being replaced with forEach loop
@@ -17,6 +31,24 @@ for (let i = 0; i < pokemonList.length; i++) {
     }
 } */
 
+// Function to return all array items in pokemonList
+function getAll() {
+    return pokemonList;
+}
+
+//Function to add new pokemon objects to array pokemonList
+function add(newPokemon) {
+    pokemonList.push(newPokemon);
+}
+
+// IIFE returns the object below with key-value pairs associated with the functions above
+return {
+    add: add,
+    getAll: getAll
+};
+})();
+
+// forEach function instead of for loop to iterate over the Pokémon in the pokemonList array in order to print details of each one
 pokemonList.forEach(function(pokemon) {
     if (pokemonList[i].height <= 1){
         document.write("<p>" + pokemonList[i].name + " (height:" + pokemonList[i].height + ") " + "- Just a little Pokemon." + "</p>");
